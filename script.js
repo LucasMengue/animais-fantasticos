8c93aa5ed5f3bf70270685a53a8bdc5fffeb6d21
lucas <lucasmengue96@hotmail.com>
2022-04-29 22:20:21 -0300

    created animation on scroll function

--- script.js
+++ script.js
@@ -67,3 +67,25 @@ function initSmoothScroll() {
   });
 }
 initSmoothScroll();
+
+function initScrollAnimation() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const halfWindow = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - halfWindow < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initScrollAnimation();
