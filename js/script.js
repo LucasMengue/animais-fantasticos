fc7cade0a3a5341a025d146e6a3179483ca49353
lucas <lucasmengue96@hotmail.com>
2022-05-19 15:04:10 -0300

    importing and initializing the modal module

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from "./modules/scroll-suave.js";
 import initScrollAnimation from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initSmoothScroll();
 initScrollAnimation();
 initAccordion();
 initTabNav();
+initModal();
